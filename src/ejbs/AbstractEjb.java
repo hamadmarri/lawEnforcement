113df9de450800e118da5a820f8b82e39ab33cd7
hamadmarri <hamad.s.almarri@gmail.com>
2014-12-19 16:49:30 -0600

    AbstractEjb.java is documented

--- src/ejbs/AbstractEjb.java
+++ src/ejbs/AbstractEjb.java
@@ -6,21 +6,38 @@ import javax.ejb.Stateless;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 
-import entities.Relatable;
-import entities.entries.Entry;
-import entities.entries.files.EntryFile;
 import entities.entries.history.Changeable;
-import entities.events.Event;
 
 
+/**
+ * @author hamadalmarri
+ * 
+ *         this EJB class is for JPA entities. This class has the basic CRUD
+ *         functionalities
+ * 
+ * @param <T>
+ *            must be one of Relatable object children
+ */
 @Stateless
 public class AbstractEjb<T> {
+
+	// entity manager
 	@PersistenceContext(unitName = "lawEnforcementPersistenceUnit")
 	private EntityManager em;
+
+	// the type of the relatable object
+	// i.e. Persone or Conveyance
+	// this is because some operations need the
+	// type to be passed
 	protected String entityName;
 
 
 
+	/**
+	 * @param id
+	 *            of object in DB
+	 * @return Entity object
+	 */
 	@SuppressWarnings("unchecked")
 	public T getEntity(Long id) {
 		return (T) em.createNamedQuery(this.entityName + ".findById").setParameter("id", id).getResultList().get(0);
@@ -28,6 +45,13 @@ public class AbstractEjb<T> {
 
 
 
+	/**
+	 * @param id
+	 *            of object in DB
+	 * @param entityName
+	 *            specified entity name
+	 * @return Entity object
+	 */
 	@SuppressWarnings("unchecked")
 	public T getEntity(Long id, String entityName) {
 		return (T) em.createNamedQuery(entityName + ".findById").setParameter("id", id).getResultList().get(0);
@@ -35,14 +59,25 @@ public class AbstractEjb<T> {
 
 
 
+	/**
+	 * @param t
+	 *            entity to be added in DB
+	 */
 	public void add(T t) {
 		em.persist(t);
 	}
 
 
 
+	/**
+	 * Checks if the entity is Changable first to log the changes. Then, saves
+	 * changes to DB
+	 * 
+	 * @param t
+	 *            is the entity
+	 * @return the entity
+	 */
 	public T save(T t) {
-//		System.out.println("*********** " + t.getClass().getSuperclass().getSuperclass().getSuperclass());
 
 		if (t.getClass().getSuperclass() == Changeable.class
 				|| t.getClass().getSuperclass().getSuperclass() == Changeable.class
@@ -70,6 +105,9 @@ public class AbstractEjb<T> {
 
 
 
+	/**
+	 * @return List of entities
+	 */
 	@SuppressWarnings("unchecked")
 	public List<T> getList() {
 		return em.createNamedQuery(this.entityName + ".findAll").getResultList();
@@ -77,6 +115,11 @@ public class AbstractEjb<T> {
 
 
 
+	/**
+	 * @param entityName
+	 *            specified entity name
+	 * @return List of entities
+	 */
 	@SuppressWarnings("unchecked")
 	public List<T> getListByEntityName(String entityName) {
 		return em.createNamedQuery(entityName + ".findAll").getResultList();
@@ -84,6 +127,11 @@ public class AbstractEjb<T> {
 
 
 
+	/**
+	 * @param type
+	 *            specific type of entities
+	 * @return List of entities
+	 */
 	@SuppressWarnings("unchecked")
 	public List<T> getList(String type) {
 		return em.createNamedQuery(this.entityName + ".findAllByType").setParameter("type", type).getResultList();
@@ -103,6 +151,12 @@ public class AbstractEjb<T> {
 
 
 
+	/**
+	 * Removes entity from DB
+	 * 
+	 * @param entityId
+	 *            id of the entity
+	 */
 	public void remove(Long entityId) {
 		T entity = this.getEntity(entityId);
 		em.remove(entity);
