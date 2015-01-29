dc29a2ba415e5c716daa2332bbc9d0e53e03d903
hamadmarri <hamad.s.almarri@gmail.com>
2015-01-29 00:03:11 -0600

    working on offender dependancies, finished 1,2,3,4,5,6,7,8,9,10,11,12

--- src/entities/intelligence/FillUpDataBase.java
+++ src/entities/intelligence/FillUpDataBase.java
@@ -19,16 +19,255 @@ public class FillUpDataBase {
 	@PostConstruct
 	public void fill() {
 
-		CrimeScene sc = new CrimeScene(true, true, true, true, false, true, true, true, true, true, true, true, true,
-				true, true, true, true, true, false, true, true, true, true, true, false, true, true, true, true, true,
-				true, true, true, true, true, true);
+		loop1000Times();
 
-		OffenderProfile op = new OffenderProfile(true, false, true, true, true, true, false, true, true, true, true,
-				true, true, true, true, true, true, true, false, true, true);
-
-		sc.setOffenderProfile(op);
-
-		em.persist(sc);
+		// sc.setOffenderProfile(op);
+		// em.persist(sc);
 	}
+
+
+
+	public void loop1000Times() {
+		CrimeScene cs;
+		OffenderProfile op;
+		int numberOfOffenderVariables = 0; // max is 4
+
+		for (int i = 0; i < 10; i++) {
+			cs = new CrimeScene(false, false, false, false, false, false, false, false, false, false, false, false,
+					false, false, false, false, false, false, false, false, false, false, false, false, false, false,
+					false, false, false, false, false, false, false, false, false, false);
+
+			op = new OffenderProfile(false, false, false, false, false, false, false, false, false, false, false,
+					false, false, false, false, false, false, false, false, false, false);
+
+			numberOfOffenderVariables = getNumberOfOffenderVariables();
+
+			createRecord(cs, op, numberOfOffenderVariables);
+
+		}
+
+	}
+
+
+
+	public void createRecord(CrimeScene cs, OffenderProfile op, int numberOfOffenderVariables) {
+		int randomOffenderVariable = 0; // max 21
+		int oldRandom = 0;
+
+		for (int j = 0; j < numberOfOffenderVariables; j++) {
+
+			while (oldRandom == randomOffenderVariable)
+				randomOffenderVariable = getRandomOffenderVariable();
+
+			oldRandom = randomOffenderVariable;
+
+			switch (randomOffenderVariable) {
+			case 1: // Prior theft
+				priorTheft(cs, op);
+				break;
+
+			case 2: // Prior burglary
+				priorBurglary(cs, op);
+				break;
+
+			case 3: // Prior violence
+				priorViolence(cs, op);
+				break;
+
+			case 4: // Prior damage
+				priorDamage(cs, op);
+				break;
+
+			case 5: // Prior disorder
+				priorDisorder(cs, op);
+				break;
+
+			case 6: // Prison
+				recordOfImprisonment(cs, op);
+				break;
+
+			case 7:
+				youngOffender(cs, op);
+				break;
+
+			case 8:
+				unemployedAtTheTimeOfOffense(cs, op);
+				break;
+
+			case 9:
+				historyOfSexCrime(cs, op);
+				break;
+				
+			case 10:
+				op.setArmedServices_PastOrPresent(true);
+				break;
+				
+			case 11:
+				op.setFamiliarWithAreaOfOffenseOccurrence(true);
+				break;
 	
+			case 12:
+				male(cs, op);
+				break;
+			default:
+				break;
+			}
+		}
+	}
+
+
+
+	public void priorTheft(CrimeScene cs, OffenderProfile op) {
+		op.setCriminalRecordOfTheft(true);
+		priorBurglary(cs, op);
+		unemployedAtTheTimeOfOffense(cs, op);
+		turnedSelfIntoPolice(cs, op);
+
+		cs.setVictimWasBlindfolded(true);
+		cs.setIdentifiablePropertyStolen(true);
+		cs.setNonidentifiablePropertyStolen(true);
+	}
+
+
+
+	public void priorBurglary(CrimeScene cs, OffenderProfile op) {
+		op.setCriminalRecordOfBurglary(true);
+		priorViolence(cs, op);
+		priorDamage(cs, op);
+		recordOfImprisonment(cs, op);
+		sexualRelatedCriminalRecord(cs, op);
+		male(cs, op);
+		cs.setValuablePropertyStolen(true);
+	}
+
+
+
+	public void priorViolence(CrimeScene cs, OffenderProfile op) {
+		op.setCriminalRecordOfViolence(true);
+		priorDamage(cs, op);
+		cs.setDeliberateClothingDamaged(true);
+		cs.setWoundsCausedByBluntInstrument(true);
+	}
+
+
+
+	public void priorDamage(CrimeScene cs, OffenderProfile op) {
+		op.setCriminalRecordOfCommittingDamage(true);
+		priorDisorder(cs, op);
+		bloodRelatedToVictim(cs, op);
+	}
+
+
+
+	public void priorDisorder(CrimeScene cs, OffenderProfile op) {
+		op.setCriminalRecordOfDisorderlyConduct(true);
+	}
+
+
+
+	public void youngOffender(CrimeScene cs, OffenderProfile op) {
+		op.setYoungOffenderBetween17And21Years(true);
+		op.setArmedServices_PastOrPresent(true);
+		priorFraud(cs, op);
+
+		cs.setAnalPenetration(true);
+		cs.setForeignObjectPenetration(true);
+		cs.setBound(true);
+	}
+
+
+
+	public void priorFraud(CrimeScene cs, OffenderProfile op) {
+		// TODO Auto-generated method stub
+
+	}
+
+
+
+	public void male(CrimeScene cs, OffenderProfile op) {
+		op.setMale(true);
+		
+		cs.setFaceNotDeliberatelyHidden(true);
+		cs.setWoundsCausedByBluntInstrument(true);
+		cs.setSexualCrime(true);
+		cs.setVictimDruggedAndOrPoisoned(true);
+		cs.setVictimCovered(true);
+	}
+
+
+
+	public void sexualRelatedCriminalRecord(CrimeScene cs, OffenderProfile op) {
+		// TODO Auto-generated method stub
+
+	}
+
+
+
+	public void recordOfImprisonment(CrimeScene cs, OffenderProfile op) {
+		op.setRecordOfImprisonment(true);
+		historyOfSexCrime(cs, op);
+		male(cs, op);
+
+		cs.setForeignObjectPenetration(true);
+	}
+
+
+
+	public void unemployedAtTheTimeOfOffense(CrimeScene cs, OffenderProfile op) {
+		op.setUnemployedAtTheTimeOfOffense(true);
+		op.setFamiliarWithAreaOfOffenseOccurrence(true);
+
+		cs.setGunshotWounds(true);
+		cs.setSuffocation(true);
+	}
+
+
+
+	public void historyOfSexCrime(CrimeScene cs, OffenderProfile op) {
+		op.setHistoryOfAbusivenessInPastRelationships(true);
+		knewVictim(cs, op);
+	}
+
+
+
+	public void knewVictim(CrimeScene cs, OffenderProfile op) {
+		// TODO Auto-generated method stub
+		
+	}
+
+
+
+	public void bloodRelatedToVictim(CrimeScene cs, OffenderProfile op) {
+		// TODO Auto-generated method stub
+
+	}
+
+
+
+	public void turnedSelfIntoPolice(CrimeScene cs, OffenderProfile op) {
+		// TODO Auto-generated method stub
+	}
+
+
+
+	public int getNumberOfOffenderVariables() {
+		int n = (int) (Math.random() * 10);
+
+		n = n % 2;
+		n++;
+
+		return n;
+	}
+
+
+
+	public int getRandomOffenderVariable() {
+		int n = (int) (Math.random() * 100);
+
+		n = n % 21;
+		n++;
+
+		return n;
+	}
+
 }
