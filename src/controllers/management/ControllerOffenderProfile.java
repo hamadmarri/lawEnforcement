64269587e809a2e93e13e17ca9925e890b2c81f5
hamadmarri <hamad.s.almarri@gmail.com>
2015-03-21 00:59:04 -0600

    view offender profile page's interface is done with showing the suspects in table

--- src/controllers/management/ControllerOffenderProfile.java
+++ src/controllers/management/ControllerOffenderProfile.java
@@ -3,7 +3,6 @@ package controllers.management;
 import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.Calendar;
-import java.util.Collection;
 import java.util.Collections;
 import java.util.Comparator;
 import java.util.List;
